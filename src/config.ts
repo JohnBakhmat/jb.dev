46de5d211706158ee33165ed7d1fcd0ec5c6590e
JohnBakhmat <johnbakhmat@gmail.com>
2023-07-21 21:30:40 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "nexxel •";
-export const SITE_DESCRIPTION = "17 yo self-taught developer";
+export const SITE_TITLE = "johnbakhmat";
+export const SITE_DESCRIPTION = "Software Developer"
